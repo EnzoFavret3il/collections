f68dfb0c6ede1e6219098f25a3ed430b00196c6c
Enzo Favret <151365706+EnzoFavret3il@users.noreply.github.com>
2024-03-01 09:16:49 +0100

    Update ListeTableau.java

--- src/exo1/ListeTableau.java
+++ src/exo1/ListeTableau.java
@@ -5,16 +5,35 @@ public class ListeTableau<T> {
 	private int taille;
 	private static final int TAILLE_DEFAUT=10;
 	
-	ListeTableau(Object[] tab, int taille){
-		this.tab=new Object[TAILLE_DEFAUT];
-		this.taille=0;
+	ListeTableau(int taille){
+		this.tab=new Object[taille];
+		this.taille=taille;
 	}
 	public int taille() {
 		return taille;
 	}
 	
+	public ListeTableau() {
+		this(TAILLE_DEFAUT);
+	}
 	
-	public void ajouter(Object element) {
-		
+	public void ajouter(T element) {
+		verifierTaille();
+		tab[taille++] = element;
+	}
+	
+	public T get(int index) {
+		if(index < 0 || index>=taille)
+			throw new IndexOutOfBoundsException(index);
+		return (T) tab[index];
+	}
+	public void verifierTaille() {
+		if(taille==tab.length) {
+			Object[]tmp = new Object[2*taille];
+			for(int i = 0 ; i<taille;i++) {
+				tmp[i]=tab[i];
+			}
+			tab=tmp;
+		}
 	}
 } 
