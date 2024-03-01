5e2fea9b2ee339356cfe7ffec6f5054a2adcc131
Enzo Favret <151365706+EnzoFavret3il@users.noreply.github.com>
2024-03-01 09:09:14 +0100

    Update ListeTableau.java

--- src/exo1/ListeTableau.java
+++ src/exo1/ListeTableau.java
@@ -1,12 +1,19 @@
 package exo1;
 
-public class ListeTableau {
+public class ListeTableau<T> {
 	private Object[] tab;
 	private int taille;
+	private static final int TAILLE_DEFAUT=10;
 	
 	ListeTableau(Object[] tab, int taille){
-		this.tab=new Object[taille];
+		this.tab=new Object[TAILLE_DEFAUT];
+		this.taille=0;
 	}
+	public int taille() {
+		return taille;
+	}
+	
+	
 	public void ajouter(Object element) {
 		
 	}
