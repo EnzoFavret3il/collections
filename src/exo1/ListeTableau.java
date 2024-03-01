494d4d0c3371fbc7859ab7ba09ddddfae27994e0
Enzo Favret <151365706+EnzoFavret3il@users.noreply.github.com>
2024-03-01 09:04:59 +0100

    JAVACODE

---
+++ src/exo1/ListeTableau.java
@@ -0,0 +1,13 @@
+package exo1;
+
+public class ListeTableau {
+	private Object[] tab;
+	private int taille;
+	
+	ListeTableau(Object[] tab, int taille){
+		this.tab=new Object[taille];
+	}
+	public void ajouter(Object element) {
+		
+	}
+} 
