00fccf326fadc3bdd985c69817ab0ca2dac21fae
Enzo Favret <151365706+EnzoFavret3il@users.noreply.github.com>
2024-03-04 10:40:06 +0100

    Code Change

--- src/Association/Annuaire.java
+++ src/Association/Annuaire.java
@@ -1,8 +1,34 @@
 package Association;
 import java.util.* ;
+import java.util.Map.Entry;
 
 
-public class Annuaire<A> {
+public class Annuaire {
 
-	Map<String, String> langues = new HashMap<>(); 
+	HashMap<String, FicheAnnuaire> annuaire; 
+	
+	public Annuaire() {
+		annuaire = new HashMap();
+	}
+	
+	public void ajouterFicher(String nom, FicheAnnuaire fiche) {
+		annuaire.put(nom, fiche);
+	}
+	public FicheAnnuaire getFiche(String nom){
+		
+		return annuaire.get(nom);
+		
+	}
+	
+	public void afficherToutesLesFiches() {
+		StringBuilder sb = new StringBuilder();
+		for(Entry<String, FicheAnnuaire> entry : annuaire.entrySet()) {
+			sb.append(entry.getKey())
+			.append(":\n")
+			.append(entry.getValue())
+			.append('\n')
+			.append('\n');
+		}
+		System.out.println(sb);
+	}
 }
--- src/liste/ListeTriee.java
+++ src/liste/ListeTriee.java
@@ -11,13 +11,12 @@ public class ListeTriee {
         listeTriee = new ArrayList<>();
     }
 
-    // Méthode pour ajouter un nouvel élément à la liste tout en maintenant l'ordre
+
     public void ajoute(String element) {
         listeTriee.add(element);
-        Collections.sort(listeTriee); // Trie la liste après l'ajout
-    }
+        Collections.sort(listeTriee); 
+        }
 
-    // Méthode pour afficher les éléments de la liste
     public void affiche() {
         for (String element : listeTriee) {
             System.out.println(element);
